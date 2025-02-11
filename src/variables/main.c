339c56c95853c68780643db51fc1053191475419
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-02-10 21:22:20 -0300

    chore: init

---
+++ src/variables/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* global variables */
+int a = 10;
+int length = 0;
+
+/* const variable */
+const int b = 10;
+
+/* its wrong */
+/* char str = "hello"; */
+/* its need to be an array */
+const char str[] = "Hello"; /* the size of the string is determined by the compiler automatically */
+/* OR */
+const char str2[] = {'H', 'e', 'l' , 'l', 'o', '\0'}; /* the '\0' is a null character*/
+
+const volatile unsigned char *port = 0x30;
+
+int funcInt () {
+    /* the keyword 'auto' is not necessary */
+    /* auto int x; */
+    int x;
+
+    x = 10;
+    return x;
+}
+
+int funcIntIncrement() {
+    static int i = 0;
+    i++;
+
+    return i;
+}
+
+/*
+    
+    will return 1 (true) if 's' contains 'c' or 0 (false) if not contains
+
+*/
+int findChar(const char *s,const char c) {
+    while (*s) {
+        if (*s == c) return true; 
+        else s++;
+    }
+
+    return false;
+}
+
+bool isTrue() {
+    bool b = true; /* <stdbool.h> */
+    return b;
+}
+
+int main() {
+    // Format specifiers define the type of data 
+    // to be printed on standard output. 
+    // You need to use format specifiers whether you're printing 
+    // formatted output with printf() or accepting input with scanf().
+    // printf("%d", funcInt()); // the '%d' is for a decimal integer (assumes base 10)
+    
+    printf("%d\n", findChar("hello", 'w'));
+    printf("%s", str2);
+
+    return 0;
+}
