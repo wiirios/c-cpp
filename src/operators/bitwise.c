652a42436532566e5a83e9a84d8dd4f111a93f47
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-03-08 22:25:14 -0300

    feat: add some methods

--- src/operators/bitwise.c
+++ src/operators/bitwise.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define LENGTH 6
+
 void decToBinary(int number) { 
     char result[32];
     int i = 0;
@@ -31,13 +33,26 @@ int countBits(int number) {
     return count;
 }
 
+int findMissing(int array[], int k) {
+    int missing = 0;
+
+    for (int i = 0; i < k; i++) missing ^= i;
+
+    for (int i = 0; i < k - 1; i++) missing ^= array[i]; 
+
+    return missing;
+}
+
 bool isTheSame(int number1, int number2) {
     return (-(~ (number1 & number2) + 1)) == number1;
 }
 
 int main() {
+    int array[] = {0, 1, 2, 3, 5};
+
     printf("%d\n", countBits(637));
-    printf("%d", isTheSame(100, 100));
+    printf("%d\n", isTheSame(100, 100));
+    printf("%d", findMissing(array, LENGTH));
 
     return 0;
 }
