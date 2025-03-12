8422a1e83edde512624803f6f233aebf8e11df6c
Winnd11 <recuperacaowilliamrios@gmail.com>
2025-03-12 10:25:41 -0300

    test: hmm

---
+++ test/build/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/main.c.obj"
+  "CMakeFiles/c.dir/main.c.obj.d"
+  "c.exe"
+  "c.exe.manifest"
+  "c.pdb"
+  "libc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/build/CMakeFiles/c.dir/main.c.obj.d
@@ -0,0 +1,8 @@
+CMakeFiles/c.dir/main.c.obj: \
+ C:\Users\William\Desktop\William\c-lang\test\c\main.c \
+ c:\mingw\include\stdio.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ c:\mingw\include\sys/types.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h
